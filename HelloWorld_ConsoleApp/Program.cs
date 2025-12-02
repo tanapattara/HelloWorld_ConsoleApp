82a675f8f2ba8af0958cc6a077e75e1cbe6a70ad
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2025-12-02 10:27:41 +0700

    Initial commit

---
+++ HelloWorld_ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print to the console
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is, Tanapattara Wongkhamchan");
+            Console.WriteLine("Student ID: 65030261");
+            Console.WriteLine("Major: Computer and Information Science");
+            Console.WriteLine("Faculty: IS");
+            Console.WriteLine("Khon Kaen University");
+        }
+    }
+}
